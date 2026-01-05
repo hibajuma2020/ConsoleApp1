5bc4970b2ddd573f124c9a9d446cf6081c409c59
hiba.js <hiba.js@MEMLT0037>
2026-01-05 09:20:08 +0400

    Task A:

--- Program.cs
+++ Program.cs
@@ -103,12 +103,25 @@ switch (option)
                         Console.WriteLine("Positive numbers count: " + positiveCount);
                         Console.WriteLine("Negative numbers count: " + negativeCount);
                         break;
+
+                    case A:
+                        Console.WriteLine("Enter a number:");
+                        int N = int.Parse(Console.ReadLine());
+
+                        for (int i = 1; i <= N; i++)
+                        {
+                            for (int j = 1; j <= N; j++)
+                            {
+                                Console.Write(i * j + " ");
+                            }
+                            Console.WriteLine();
+                            break;
+                        }
                 }
             }
         }
-
-
     }
-}
+            }
+        
              
         
